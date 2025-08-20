c6d4c226e1fa4b18a2b408725527ceee4d7c15a8
BuildTools <unconfigured@null.spigotmc.org>
2025-08-20 22:24:41 +0200

    General update

--- examples/02_changing_candle_kind.py
+++ examples/02_changing_candle_kind.py
@@ -3,10 +3,11 @@ Example 2: Changing candlestick kinds
 
 There are two equivalent ways to change candelstick types.
 """
+from pathlib import Path
 from pricedata.io.loader import DataConfig, ClientConfig, DataLoader
 from pricedata.core.dataset import Data
 from pricedata.utils.pandas.pandas_init import pandas_set_up_func
-from pathlib import Path
+from pricedata.utils.dev_types.spec.spec import OHLCSpec, DropColumnsSpec, ReturnSpec
 
 
 # set up pandas display range (for visual purposes only)
@@ -14,7 +15,7 @@ pandas_set_up_func(is_max_cols=True)
 
 # initialize data configuration
 data_config = DataConfig(
-    symbol="TVC:NDQ",
+    symbol="INDEX:BTCUSD",
     interval='1d',
     n_bars=1000,
     base_dir=Path(__file__).parent.parent / "data"  # to save downloaded data if it does not exist
@@ -34,18 +35,34 @@ loader = DataLoader()
 # The first way
 print("The first way.")
 data = Data(data_config, client_config, loader)
-data.load().with_candles(kind='ha')
+data.load().with_candles(kind='ha', append=False)
 
 # print the results
 print(data.df.head(10))
 
-
+del data
 # -------------------------------------------
 # The second way
 print("The second way.")
 data = Data(data_config, client_config, loader)
 data.load()
-data.with_candles(kind='ha')
+data.with_candles(kind='ha', append=True)
+
+spec = OHLCSpec(
+    feature_kinds=["ohlc4", "hlc-3", "hl2", "hlcc4"],
+    candle_kinds=["s", "ha"]
+)
+data.with_features(spec)
+
+spec = DropColumnsSpec(cols=["s", "v"])
+data.drop_columns(spec=spec)
+
+spec = ReturnSpec(
+    feature_kinds="r",
+    sources="close"
+)
+print(spec)
+data.with_features(spec)
 
 # print the results
-print(data.df.head(10))
+print(data.df.iloc[0])

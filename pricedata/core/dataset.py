f1c226e9c31f7c4a2fd427db988bb391bf122e06
BuildTools <unconfigured@null.spigotmc.org>
2025-08-20 22:32:32 +0200

    Dataset update

--- pricedata/core/dataset.py
+++ pricedata/core/dataset.py
@@ -1,21 +1,57 @@
-import pandas as pd
 from pricedata.io.loader import DataLoader, DataConfig, ClientConfig
 from pricedata.transforms.candles import to_heikin_ashi
+from pricedata.transforms.features import *
+from pricedata.utils.dev_types.spec.spec import OHLCSpec, DropColumnsSpec, ReturnSpec
+from pricedata.utils.dev_types.dev_types import ColumnTypeEnum
 
 
 class Data:
-    def __init__(self, cfg: DataConfig, client_cfg: ClientConfig, loader: DataLoader):
-        self._cfg = cfg
+    """
+    The main object to operate on the price data.
+    """
+    def __init__(self, data_cfg: DataConfig, client_cfg: ClientConfig, loader: DataLoader):
+        """
+        Setting initialize parameters.
+
+        Args:
+            data_cfg (DataConfig): contextual parameters
+            client_cfg (ClientConfig): client username and password
+            loader (DataLoader): price data loader object
+        """
+        self._data_cfg = data_cfg
         self._client_cfg = client_cfg
         self._loader = loader
         self._df: pd.DataFrame | None = None
 
+        self.feature_handler = {
+            ColumnTypeEnum.OHLC4: add_ohlc4,
+            ColumnTypeEnum.HLC3: add_hlc3,
+            ColumnTypeEnum.HLCC4: add_hlcc4,
+            ColumnTypeEnum.HL2: add_hl2,
+            ColumnTypeEnum.RETURN: add_return,
+            ColumnTypeEnum.LOG_RETURN: add_log_return,
+        }
+
     def load(self) -> "Data":
-        self._df = self._loader.load_or_fetch(self._cfg, self._client_cfg)
+        """
+        Download and save data fom TradingView via tvDatafeed or load from file.
+
+        Specify the save and read path in DataConfig.
+
+        Return:
+            Self
+        """
+        self._df = self._loader.load_or_fetch(self._data_cfg, self._client_cfg)
         return self
 
     @property
     def df(self) -> pd.DataFrame:
+        """
+        Get price data as DataFrame.
+
+        Return:
+            The price data
+        """
         if self._df is None:
             raise RuntimeError("Call Data.load() first")
         return self._df
@@ -42,11 +78,25 @@ class Data:
             raise ValueError(f"Unknown candle type: {kind}")
         return self
 
-    def with_features(self) -> "Data":
+    def with_features(self, spec: OHLCSpec | ReturnSpec = None) -> "Data":
+        """
+        Allows you to add columns such as:
+        OHLC4, HLC3, return, logarithmic return
+
+        
+        """
+        for feature_kind in spec.feature_kinds:
+            handler_ = self.feature_handler[feature_kind]
+            if handler_:
+                handler_(self._df, spec=spec)
+
         return self
 
     def with_states(self) -> "Data":
         return self
 
+    def drop_columns(self, spec: DropColumnsSpec):
+        self._df = self._df.drop(columns=spec.cols)
+
     def save(self) -> None:
-        self._loader.save(self.df, self._cfg)
+        self._loader.save(self.df, self._data_cfg)
